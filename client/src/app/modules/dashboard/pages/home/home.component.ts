eb526c34081de0b53d8b59a38d2ffa8b56aced83
moein-bld <moein.bolidhe82@gmail.com>
2023-05-22 22:08:43 +0330

    update user

--- client/src/app/modules/dashboard/pages/home/home.component.ts
+++ client/src/app/modules/dashboard/pages/home/home.component.ts
@@ -11,34 +11,24 @@ export interface UserData {
 	email: string;
 	status: boolean;
 	date: string;
+	role: 'User' | 'Admin';
 }
 
 export interface UserGroup {
 	users: UserData[];
 }
 
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = ['blueberry', 'lychee', 'kiwi', 'mango', 'peach', 'lime', 'pomegranate', 'pineapple'];
-const NAMES: string[] = [
-	'Maia',
-	'Asher',
-	'Olivia',
-	'Atticus',
-	'Amelia',
-	'Jack',
-	'Charlotte',
-	'Theodore',
-	'Isla',
-	'Oliver',
-	'Isabella',
-	'Jasper',
-	'Cora',
-	'Levi',
-	'Violet',
-	'Arthur',
-	'Mia',
-	'Thomas',
-	'Elizabeth',
+const userDataSource: UserData[] = [
+	{ id: 1, username: 'Maia', email: 'test-1@gmail.com', status: false, date: '11/9/2022', role: 'User' },
+	{ id: 2, username: 'Olivia', email: 'test-2@gmail.com', status: false, date: '1/10/2022', role: 'User' },
+	{ id: 3, username: 'Amelia', email: 'test-3@gmail.com', status: true, date: '11/6/2022', role: 'User' },
+	{ id: 4, username: 'Jack', email: 'test-4@gmail.com', status: false, date: '6/19/2022', role: 'User' },
+	{ id: 5, username: 'Isla', email: 'test-5@gmail.com', status: true, date: '9/11/2022', role: 'User' },
+	{ id: 6, username: 'Oliver', email: 'test-6@gmail.com', status: false, date: '3/1/2022', role: 'User' },
+	{ id: 7, username: 'Jasper', email: 'test-7@gmail.com', status: true, date: '3/2/2022', role: 'User' },
+	{ id: 8, username: 'Violet', email: 'test-8@gmail.com', status: false, date: '4/17/2022', role: 'User' },
+	{ id: 9, username: 'Mia', email: 'test-9@gmail.com', status: false, date: '9/9/2022', role: 'User' },
+	{ id: 10, username: 'Elizabeth', email: 'test-10@gmail.com', status: false, date: '1/9/2022', role: 'User' },
 ];
 
 @Component({
@@ -47,59 +37,33 @@ const NAMES: string[] = [
 	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements AfterViewInit {
-	displayedColumns: string[] = ['id', 'username', 'email', 'status', 'date'];
+	displayedColumns: string[] = ['id', 'username', 'email', 'role', 'status', 'date', 'actions'];
 	dataSource: MatTableDataSource<UserData>;
 	type: string;
 	show: boolean;
 	userGroupForm: FormGroup;
-	conditions = [
-		{
-			value: true,
-			viewValue: 'active',
-		},
-		{
-			value: false,
-			viewValue: 'Inactive',
-		},
-	];
+	counter: number;
 
 	@ViewChild(MatPaginator) paginator!: MatPaginator;
 	@ViewChild(MatSort) sort!: MatSort;
 
 	constructor(public dialog: MatDialog, private fb: FormBuilder) {
-		// Create 10 users
-		const users = Array.from({ length: 12 }, (_, k) => createNewUser(k + 1));
-
 		// Assign the data to the data source for the table to render
-		this.dataSource = new MatTableDataSource(users);
+		this.dataSource = new MatTableDataSource(userDataSource);
 
 		this.type = '';
 
 		this.show = false;
 
-		this.userGroupForm = this.fb.group({
-			users: this.fb.array([this.createUser()]),
-		});
-	}
+		this.counter = 10;
 
-	createUser(): FormGroup {
-		return this.fb.group({
-			id: Math.floor(Math.random() * 100),
-			username: ['', Validators.required],
-			email: ['', [Validators.required, Validators.email]],
-			status: true,
-			date: new Date().toLocaleDateString(),
+		this.userGroupForm = this.fb.group({
+			users: this.fb.array([this.createForm()]),
 		});
 	}
 
 	get usersForm(): FormArray {
 		return this.userGroupForm.get('users') as FormArray;
-	}
-
-	resetItems() {
-		const items = this.userGroupForm.get('users') as FormArray;
-		items.clear()
-		items.push(this.createUser())
 	}
 
 	ngAfterViewInit() {
@@ -119,33 +83,74 @@ export class HomeComponent implements AfterViewInit {
 	openCreate() {
 		this.type = 'create';
 		this.show = true;
+		this.resetItems()
+	}
+
+	openEdit(row: UserData) {
+		const isExist = this.usersForm.controls.some(item => item.value.id === row.id);
+		if (isExist) return;
+
+		if (this.type === 'create' || this.type === '') this.usersForm.clear();
+
+		this.type = 'edit';
+		this.show = true;
+		this.usersForm.push(this.createForm(row));
 	}
 
 	addRow(event: Event) {
 		event.preventDefault();
-		if (this.usersForm.length < 20) this.usersForm.push(this.createUser());
+		if (this.usersForm.length < 20) this.usersForm.push(this.createForm());
 	}
 
-	createOrUpdate() {
-		if (this.usersForm.valid) {
-			this.usersForm.value.forEach((item: UserData) => {
-				this.dataSource.data.push(item);
+	createForm(row?: UserData): FormGroup {
+		if (row)
+			return this.fb.group({
+				id: row.id,
+				username: [row.username, Validators.required],
+				email: [row.email, [Validators.required, Validators.email]],
+				status: row.status,
+				date: row.date,
+				role: row.role,
 			});
-			this.dataSource._updateChangeSubscription();
-			this.resetItems()
+
+		this.counter++;
+		return this.fb.group({
+			id: this.counter,
+			username: ['', Validators.required],
+			email: ['', [Validators.required, Validators.email]],
+			status: true,
+			date: new Date().toLocaleDateString(),
+			role: 'User',
+		});
+	}
+
+	resetItems() {
+		this.usersForm.clear();
+		this.usersForm.push(this.createForm());
+	}
+
+	createOrUpdate(type: string) {
+		if (this.usersForm.valid) {
+			if (type === 'edit') {
+				const map = new Map();
+
+				this.dataSource.data = this.dataSource.data.concat(this.usersForm.value);
+
+				for (const tag of this.dataSource.data) {
+					map.set(tag.id, tag);
+				}
+
+				const final = [...map.values()];
+				this.dataSource.data = final;
+
+				this.dataSource._updateChangeSubscription();
+			} else if (type === 'create') {
+				this.usersForm.value.forEach((item: UserData) => {
+					this.dataSource.data.push(item);
+				});
+				this.dataSource._updateChangeSubscription();
+				this.resetItems();
+			}
 		}
 	}
-}
-
-function createNewUser(id: number): UserData {
-	const startDate = new Date('2022-01-01');
-	const endDate = new Date('2023-01-01');
-	const randomTime = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime())).toLocaleDateString();
-	return {
-		id: id,
-		username: NAMES[Math.round(Math.random() * (FRUITS.length - 1))],
-		email: `test-${id}@gmail.com`,
-		status: Boolean(Math.round(Math.random())),
-		date: randomTime,
-	};
 }
